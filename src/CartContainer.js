f68c6654898336043cf743daac41822a64cb2e6d
HashimAziz <hashimkayani97@gmail.com>
2021-06-18 08:32:33 +0500

    components boiler plates added

---
+++ src/CartContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CartContainer = () => {
+  return <h2>CartContainer</h2>;
+};
+
+export default CartContainer;
---
+++ src/CartItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CartItem = () => {
+  return <h2>CartItem</h2>;
+};
+
+export default CartItem;
