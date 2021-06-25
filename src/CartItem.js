fd94acdec1364fbc9627d379b1680c85d2b283b6
HashimAziz <hashimkayani97@gmail.com>
2021-06-25 09:05:02 +0500

    working with cartitem component

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { useGlobalContext } from "./context";
 
-const CartItem = () => {
-  return <h2>CartItem</h2>;
+const CartItem = ({ id, img, title, price, amount }) => {
+  return (
+    <article className="cart-item">
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">${price}</h4>
+        {/* remove particular item button */}
+        <button className="remove-btn">remove</button>
+      </div>
+      <div>
+        {/* incease amount button */}
+        <button className="amount-btn">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z" />
+          </svg>
+        </button>
+        <p className="amount">{amount}</p>
+        {/* decrease amount button */}
+        <button className="amount-btn">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+          </svg>
+        </button>
+      </div>
+    </article>
+  );
 };
 
 export default CartItem;
